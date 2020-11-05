f864f0556c76ed7875734666324925f282c0bb57
harshavardhanak <53828700+harshavardhanak@users.noreply.github.com>
2020-11-05 15:11:12 +0530

    Add files via upload

---
+++ Lab5/vowel.sh
@@ -0,0 +1,52 @@
+echo " Enter the character : "
+while :
+do
+	read a
+	case $a in 
+		"a")
+			echo " It is vowel"
+			break 
+			;;
+		"e")
+			echo " it is a vowel "
+			break
+			;;
+		"i")
+			echo " It is a vowel "
+			break
+			;;
+		"o")
+			echo " It is a vowel "
+			break
+			;;
+		"u")
+			echo "It is a vowel "
+			break
+			;;
+		"A")
+			echo " It is a vowel"
+			break
+			;;
+		"E")
+			echo "It is a vowel "
+			break
+			;;
+		"I")
+			echo " It is a vowel "
+			break
+			;;
+		"O")
+			echo " It is a vowel "
+			break
+			;;
+		"U")
+			echo " It is a vowel "
+			break
+			;;
+		*)
+			echo " It is constant"
+			break
+			esac
+		done
+	echo " That's it " 
+	
